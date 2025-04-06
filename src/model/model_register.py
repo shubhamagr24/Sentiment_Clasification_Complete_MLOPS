ad4217d84b9049de9dde6e3fd4f7a94294653773
shubhamagr24 <shubhamagr24@gmil.com>
2025-04-06 14:38:39 +0530

    CICD run-1

--- src/model/model_register.py
+++ src/model/model_register.py
@@ -14,26 +14,26 @@ warnings.filterwarnings("ignore")
 
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
-# Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
-# -------------------------------------------------------------------------------------
+dagshub_url = "https://dagshub.com"
+repo_owner = "shubhamagr24"
+repo_name = "entiment_Clasification_Complete_MLOPS"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+-------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/shubhamagr24/Sentiment_Clasification_Complete_MLOPS.mlflow')
-dagshub.init(repo_owner='shubhamagr24', repo_name='Sentiment_Clasification_Complete_MLOPS', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/shubhamagr24/Sentiment_Clasification_Complete_MLOPS.mlflow')
+# dagshub.init(repo_owner='shubhamagr24', repo_name='Sentiment_Clasification_Complete_MLOPS', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
